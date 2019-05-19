8665119cc524b79bf6b21a7b5174a4acec88c7d7
Wanderson <wandersonduarte.br@gmail.com>
2019-05-19 18:46:50 -0300

    Implementado o agendador de tarefas

--- src/main/java/br/com/singra/SingraWmsBatchApplication.java
+++ src/main/java/br/com/singra/SingraWmsBatchApplication.java
@@ -2,6 +2,8 @@ package br.com.singra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 public class SingraWmsBatchApplication {
---
+++ src/main/java/br/com/singra/schenduler/Agendador.java
@@ -0,0 +1,58 @@
+package br.com.singra.schenduler;
+
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class Agendador {
+	
+	@Autowired
+	public JobLauncher jobLauncher;
+	
+	@Autowired
+	public Job job;
+	
+	/* Nota: cron = "A B C D E F" 
+	 * A - segundos (0-59)
+	 * B - minutos (0-59)
+	 * C - horas (0-23)
+	 * D - dias  (1-31)
+	 * E - mês (1-12)
+	 * F - Dias da semana (0-6)
+	 * "\*" - siguinifica qualquer valor */
+//	  "*\numero - numero = quantide de incremento"
+	 
+	//executará a cada 10 segundos
+	@Scheduled(cron="*/10 * * * * *")
+	public void Agendador() {
+		JobParameters jobParameters = new JobParameters();
+		try {
+			JobExecution exec = jobLauncher.run(job, jobParameters);
+		} catch (JobExecutionAlreadyRunningException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JobRestartException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JobInstanceAlreadyCompleteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JobParametersInvalidException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+
+}
